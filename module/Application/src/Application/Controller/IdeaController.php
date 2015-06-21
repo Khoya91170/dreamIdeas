2bac7a90810c66544c14759da746bcca84919463
InesRg <irouag@gmail.com>
2015-06-21 03:56:09 +0200

    Add Idea

--- module/Application/src/Application/Controller/IdeaController.php
+++ module/Application/src/Application/Controller/IdeaController.php
@@ -5,3 +5,23 @@
  * Date: 21/06/2015
  * Time: 03:04
  */
+
+
+namespace Application\Controller;
+
+use Application\Model\Idea;
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IdeaController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        return;
+    }
+
+    public function addAction(){
+
+    return;
+    }
+}
